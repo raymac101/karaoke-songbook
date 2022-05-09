81c253457d71dc9672e87a7d787c8194e984f91a
Ray MacDonald <raymac@reckageinteractive.com>
2022-05-09 10:59:32 -0600

    more updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/karaoke-songbook'));
+app.use(express.static('./dist/karaoke-songbook'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/karaoke-songbook/index.html'));});
+res.sendFile(path.join('./dist/karaoke-songbook/index.html'));});
 app.listen(process.env.PORT || 8080);
