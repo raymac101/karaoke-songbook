8cea29034e71a9c91c5cf2e5fb9a980df81f552a
Ray MacDonald <raymac@reckageinteractive.com>
2022-05-09 11:20:18 -0600

    Server update

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
+
 app.use(express.static('./dist/karaoke-songbook'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join('./dist/karaoke-songbook/index.html'));});
+  res.sendFile(path.join('./dist/karaoke-songbook/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
